af2a24bb2385ecfb16e11c9583bf8eef96afcd91
sachinsharma263 <sachinsharma263@gmail.com>
2023-10-26 10:20:59 +0530

    added regpage.java

---
+++ src/main/java/com/qa/trcrm/pages/RegPage.java
@@ -0,0 +1,13 @@
+package com.qa.trcrm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegPage {
+
+	WebDriver driver;
+
+	public RegPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+}
