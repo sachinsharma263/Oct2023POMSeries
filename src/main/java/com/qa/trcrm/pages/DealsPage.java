ee63efa12baaf63fac415bb3b95ef4c76d439982
sachinsharma263 <sachinsharma263@gmail.com>
2023-10-26 08:54:42 +0530

    created dealspage.java

---
+++ src/main/java/com/qa/trcrm/pages/DealsPage.java
@@ -0,0 +1,14 @@
+package com.qa.trcrm.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.trcrm.base.BasePage;
+
+public class DealsPage extends BasePage {
+
+	WebDriver driver;
+
+	public DealsPage(WebDriver driver) {
+		this.driver = driver;
+	}
+}
